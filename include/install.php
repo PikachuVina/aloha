f021cba3863b2c782312b4dfba30ab544acc7284
PikachuVina <bmn.2312@gmail.com>
2017-05-23 00:37:05 +0700

    1

--- include/install.php
+++ include/install.php
@@ -9,4 +9,13 @@ include("../config.php");
   PRIMARY KEY (`id`) 
  ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ; 
 "); 
+@mysqli_query($GLOBALS["___mysqli_ston"], "CREATE TABLE IF NOT EXISTS `VIP` ( 
+  `idfb` bigint(21) NOT NULL AUTO_INCREMENT, 
+  `name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, 
+  `user` int(10) NOT NULL, 
+  `goi` tinyint(1) NOT NULL, 
+  `time` int(10) NOT NULL, 
+PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1; 
+");
 ?> 
