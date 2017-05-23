5e029d2760f4dd4ff4d582b2f865d83134b61e5e
PikachuVina <bmn.2312@gmail.com>
2017-05-23 07:07:41 +0700

    1

--- vip/admin.php
+++ vip/admin.php
@@ -2,7 +2,7 @@
 session_start(); 
 include '../include/config.php'; 
 include '../include/head.php'; 
-if($_SESSION['user'] == '100004294419791'){ 
+if($_SESSION['user'] == 'admin'){ 
 ?> 
 <div class="col-lg-4 col-lg-offset-4"> 
 <?php 
